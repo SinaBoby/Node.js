d0d12895712ff6da4d26580cf78a11ca54ec9fd5
SinaBoby <sinaboby91@yahoo.com>
2022-02-28 23:50:47 +0100

    server created

---
+++ homework/hackyourtemperature/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path');
+const exphbs = require('express-handlebars');
+
+const app = express();
+
+app.get('/', (req,res) => res.send('hello from backend to frontend!') )
+app.use(express.json())
+app.post('/members',(req,res) => {
+  const city = req.body.city
+  res.send(city)
+})
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Server run on port ${PORT}`))
