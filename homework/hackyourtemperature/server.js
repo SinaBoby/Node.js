0fdcaef9d29da3f62b0dfdff70031d2204773924
SinaBoby <sinaboby91@yahoo.com>
2022-03-01 00:33:53 +0100

    type module added

--- homework/hackyourtemperature/server.js
+++ homework/hackyourtemperature/server.js
@@ -1,14 +1,12 @@
-const express = require('express')
-const path = require('path');
-const exphbs = require('express-handlebars');
+import express from 'express';
 
 const app = express();
 
-app.get('/', (req,res) => res.send('hello from backend to frontend!') )
-app.use(express.json())
-app.post('/members',(req,res) => {
-  const city = req.body.city
-  res.send(city)
-})
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => console.log(`Server run on port ${PORT}`))
+app.get('/', (req, res) => res.send('hello from backend to frontend!'));
+app.use(express.json());
+app.post('/members', (req, res) => {
+  const city = req.body.city;
+  res.send(city);
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server run on port ${PORT}`));
